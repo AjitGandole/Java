79936a8b6ff1f609053162e6a3e32e101fe53516
Ajit <gandoleajit@gmail.com>
2025-07-26 22:34:01 +0530

    Personal Project

---
+++ src/main/java/com/atm/Atm.java
@@ -0,0 +1,8 @@
+package com.atm;
+
+public class Atm extends OptionMenu {
+	 public static void main(String[] args) { // Main method: entry point of the program
+	        OptionMenu options = new OptionMenu(); // Creates an object of OptionMenu class
+	        options.getLogin(); // Calls the getLogin method to prompt user login
+	    }
+}
